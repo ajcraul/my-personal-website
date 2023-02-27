d01deabf378c9add56d301a1eac89fe9de323683
allison craul <craul.aj@gmail.com>
2023-02-26 17:13:31 -0700

    updated js text content to personalize website

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
+h2.textContent = "everything there is to know about this magnificent (or average) human girl";
 
 document.querySelector ("body").appendChild(h2);
